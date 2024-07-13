ceec93307da8c545135eade88d70df228bff19d7
SmallCodeNote <132815163+SmallCodeNote@users.noreply.github.com>
2024-07-13 17:57:44 +0900

    Add files via upload

---
+++ SocketSignalClient/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace SocketSignalClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox_CLEAR = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_TEST = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label_Time = new System.Windows.Forms.Label();
+            this.label_Message = new System.Windows.Forms.Label();
+            this.textBox_statusXML = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage_Body = new System.Windows.Forms.TabPage();
+            this.tabPage_Responce = new System.Windows.Forms.TabPage();
+            this.textBox_Responce = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage_Body.SuspendLayout();
+            this.tabPage_Responce.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "0";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button2.Location = new System.Drawing.Point(74, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 54);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "0";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button3.Location = new System.Drawing.Point(136, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(56, 54);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "0";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button4.Location = new System.Drawing.Point(198, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(56, 54);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "0";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button5.Location = new System.Drawing.Point(260, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(56, 54);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "0";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 72);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(186, 50);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "CLEAR";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(204, 72);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(112, 50);
+            this.button7.TabIndex = 1;
+            this.button7.Text = "TEST";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // textBox_CLEAR
+            // 
+            this.textBox_CLEAR.Location = new System.Drawing.Point(12, 149);
+            this.textBox_CLEAR.Name = "textBox_CLEAR";
+            this.textBox_CLEAR.Size = new System.Drawing.Size(586, 19);
+            this.textBox_CLEAR.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "CLEAR";
+            // 
+            // textBox_TEST
+            // 
+            this.textBox_TEST.Location = new System.Drawing.Point(12, 193);
+            this.textBox_TEST.Name = "textBox_TEST";
+            this.textBox_TEST.Size = new System.Drawing.Size(586, 19);
+            this.textBox_TEST.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "TEST";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(332, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Message";
+            // 
+            // label_Time
+            // 
+            this.label_Time.AutoSize = true;
+            this.label_Time.Location = new System.Drawing.Point(370, 24);
+            this.label_Time.Name = "label_Time";
+            this.label_Time.Size = new System.Drawing.Size(30, 12);
+            this.label_Time.TabIndex = 4;
+            this.label_Time.Text = "Time";
+            // 
+            // label_Message
+            // 
+            this.label_Message.Location = new System.Drawing.Point(332, 36);
+            this.label_Message.Name = "label_Message";
+            this.label_Message.Size = new System.Drawing.Size(266, 86);
+            this.label_Message.TabIndex = 4;
+            this.label_Message.Text = "...";
+            // 
+            // textBox_statusXML
+            // 
+            this.textBox_statusXML.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox_statusXML.Location = new System.Drawing.Point(3, 3);
+            this.textBox_statusXML.Multiline = true;
+            this.textBox_statusXML.Name = "textBox_statusXML";
+            this.textBox_statusXML.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox_statusXML.Size = new System.Drawing.Size(611, 339);
+            this.textBox_statusXML.TabIndex = 2;
+            this.textBox_statusXML.WordWrap = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(334, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 12);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Time";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage_Body);
+            this.tabControl1.Controls.Add(this.tabPage_Responce);
+            this.tabControl1.Location = new System.Drawing.Point(12, 218);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(586, 354);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabPage_Body
+            // 
+            this.tabPage_Body.Controls.Add(this.textBox_statusXML);
+            this.tabPage_Body.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_Body.Name = "tabPage_Body";
+            this.tabPage_Body.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage_Body.Size = new System.Drawing.Size(617, 345);
+            this.tabPage_Body.TabIndex = 0;
+            this.tabPage_Body.Text = "Body";
+            this.tabPage_Body.UseVisualStyleBackColor = true;
+            // 
+            // tabPage_Responce
+            // 
+            this.tabPage_Responce.Controls.Add(this.textBox_Responce);
+            this.tabPage_Responce.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_Responce.Name = "tabPage_Responce";
+            this.tabPage_Responce.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage_Responce.Size = new System.Drawing.Size(578, 328);
+            this.tabPage_Responce.TabIndex = 1;
+            this.tabPage_Responce.Text = "Responce";
+            this.tabPage_Responce.UseVisualStyleBackColor = true;
+            // 
+            // textBox_Responce
+            // 
+            this.textBox_Responce.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox_Responce.Location = new System.Drawing.Point(3, 3);
+            this.textBox_Responce.Multiline = true;
+            this.textBox_Responce.Name = "textBox_Responce";
+            this.textBox_Responce.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox_Responce.Size = new System.Drawing.Size(572, 322);
+            this.textBox_Responce.TabIndex = 3;
+            this.textBox_Responce.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 583);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label_Message);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label_Time);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_TEST);
+            this.Controls.Add(this.textBox_CLEAR);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "SignalClient";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage_Body.ResumeLayout(false);
+            this.tabPage_Body.PerformLayout();
+            this.tabPage_Responce.ResumeLayout(false);
+            this.tabPage_Responce.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox_CLEAR;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_TEST;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label_Time;
+        private System.Windows.Forms.Label label_Message;
+        private System.Windows.Forms.TextBox textBox_statusXML;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage_Body;
+        private System.Windows.Forms.TabPage tabPage_Responce;
+        private System.Windows.Forms.TextBox textBox_Responce;
+    }
+}
+
---
+++ SocketSignalClient/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using tcpServer;
+
+using WinFormStringCnvClass;
+
+namespace SocketSignalClient
+{
+    public partial class Form1 : Form
+    {
+        private TcpSocketServer tcpSrv;
+        private CancellationTokenSource tokenSource;
+        private Task worker;
+        bool IsBusy = false;
+        int WorkerWait = 100;
+        public DateTime LastCheckTime { get; private set; }
+        public string MessageLabel
+        {
+            get { return label_Message.Text; }
+            set
+            {
+                if (this.InvokeRequired) { this.Invoke((Action)(() => MessageLabel = value)); }
+                else { label_Message.Text = value; }
+            }
+        }
+
+        public string TimeLabel
+        {
+            get { return label_Time.Text; }
+            set
+            {
+                if (this.InvokeRequired) { this.Invoke((Action)(() => TimeLabel = value)); }
+                else { label_Time.Text = value; }
+            }
+        }
+
+        string thisExeDirPath = "";
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            tokenSource = new CancellationTokenSource();
+            tcpSrv = new TcpSocketServer(80, "UTF8");
+            tcpSrv.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string paramFilename = Path.Combine(thisExeDirPath, "_param.txt");
+
+            if (File.Exists(paramFilename))
+            {
+                WinFormStringCnv.setControlFromString(this, File.ReadAllText(paramFilename));
+            }
+            tcpSrv.ResponceMessage = textBox_statusXML.Text;
+            Start();
+
+        }
+
+        public void Start()
+        {
+            if (worker == null)
+            {
+                var token = tokenSource.Token;
+                worker = Task.Run(() => Worker(token), token);
+                IsBusy = true;
+            }
+        }
+
+        public async void Stop()
+        {
+            try
+            {
+                tokenSource.Cancel();
+                Thread.Sleep(100);
+                await worker;
+                IsBusy = false;
+            }
+            catch (Exception ex)
+            {
+                //Debug.Write(GetType().Name + "::" + System.Reflection.MethodBase.GetCurrentMethod().Name);
+                //Debug.WriteLine(ex.ToString());
+            }
+        }
+
+        private void Worker(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                if ((tcpSrv.LastReceiveTime - LastCheckTime).TotalSeconds > 0 && tcpSrv.ReceivedSocketQueue.Count > 0)
+                {
+                    if (tcpSrv.ReceivedSocketQueue.TryDequeue(out string MessageString))
+                    {
+                        string[] Cols = MessageString.Replace("\r\n", "\n").Trim('\n').Split('\n')[0].Split('\t');
+
+                        if (Cols.Length >1) { MessageLabel = Cols[1]; } else { MessageLabel = MessageString; }
+                        TimeLabel = Cols[0];
+                    }
+
+                    LastCheckTime = DateTime.Now;
+                }
+                Thread.Sleep(WorkerWait);
+            }
+            token.ThrowIfCancellationRequested();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            string FormContents = WinFormStringCnv.ToString(this);
+            string paramFilename = Path.Combine(thisExeDirPath, "_param.txt");
+            File.WriteAllText(paramFilename, FormContents);
+        }
+    }
+}
